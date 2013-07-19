c7908558ea6dbfdb0e3769d960fa66228def8187
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-19 14:17:26 +0200

    Saving the branches

--- src/main/java/org/jenkins/plugin/panda/PandaProject.java
+++ src/main/java/org/jenkins/plugin/panda/PandaProject.java
@@ -1,12 +1,16 @@
 package org.jenkins.plugin.panda;
 
 import hudson.Extension;
-import hudson.model.ItemGroup;
-import hudson.model.Job;
-import hudson.model.TopLevelItem;
-import hudson.model.TopLevelItemDescriptor;
+import hudson.model.*;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
+import javax.servlet.ServletException;
+import java.io.IOException;
 import java.util.*;
 
 public class PandaProject extends Job<PandaProject, PandaRun> implements TopLevelItem {
@@ -58,6 +62,53 @@ public class PandaProject extends Job<PandaProject, PandaRun> implements TopLeve
 
     public PandaProjectDescriptor getDescriptor() {
         return DESCRIPTOR;
+    }
+
+    @Override
+    protected void submit(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException, Descriptor.FormException {
+        // Default
+        super.submit(req, rsp);
+        // Gets the JSON for the form
+        JSONObject json = req.getSubmittedForm();
+        // Main branch
+        saveMainBranch(json);
+        // Branches
+        saveBranches(json);
+    }
+
+    private void saveBranches(JSONObject json) throws Descriptor.FormException {
+        // Clears the initial list
+        branches.clear();
+        // Recreates the list
+        JSONArray jBranches = json.getJSONArray("branches");
+        Set<String> names = new HashSet<String>();
+        for (int i = 0; i < jBranches.size(); i++) {
+            JSONObject jBranch = jBranches.getJSONObject(i);
+            PandaBranch branch = asBranch(jBranch);
+            // Controls
+            String name = branch.getName();
+            if (StringUtils.isBlank(name)) {
+                throw new Descriptor.FormException(Messages.PandaBranch_Config_NameRequired(), "branches.name");
+            } else if ("default".equals(name)) {
+                throw new Descriptor.FormException(Messages.PandaBranch_Config_DefaultNotAllowed(), "branches.name");
+            } else if (names.contains(name)) {
+                throw new Descriptor.FormException(Messages.PandaBranch_Config_NameAlreadyUsed(name), "branches.name");
+            }
+            // OK
+            names.add(name);
+            branches.add(branch);
+        }
+    }
+
+    private void saveMainBranch(JSONObject json) {
+        mainBranch = asBranch(json.getJSONObject("mainBranch"));
+    }
+
+    private PandaBranch asBranch(JSONObject branch) {
+        return new PandaBranch(
+                branch.getString("name"),
+                branch.getString("description")
+        );
     }
 
     public static class PandaProjectDescriptor extends TopLevelItemDescriptor {
