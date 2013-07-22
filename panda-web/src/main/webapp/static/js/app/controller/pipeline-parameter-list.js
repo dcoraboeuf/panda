c26ad50c38d02cc8126c9dc6efb9ca2c92a3441f
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-07-22 21:57:05 +0200

    Display of the list of parameters

--- panda-web/src/main/webapp/static/js/app/controller/pipeline-parameter-list.js
+++ panda-web/src/main/webapp/static/js/app/controller/pipeline-parameter-list.js
@@ -3,7 +3,8 @@ define(['render'], function (render) {
     return {
         url: function (config) {
             return 'ui/pipeline/{0}/parameter'.format(config.pipelineId)
-        }
+        },
+        render: render.asTableTemplate('pipeline-parameter-list')
     }
 
 })
