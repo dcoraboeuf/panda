781437ee3fc4f30dbc48d3462de59f50111dcaf3
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2013-08-23 15:40:52 +0200

    Unit test for pipeline authorizations

--- panda-backend/src/test/java/net/panda/backend/security/AbstractSecurityTest.java
+++ panda-backend/src/test/java/net/panda/backend/security/AbstractSecurityTest.java
@@ -1,7 +1,9 @@
 package net.panda.backend.security;
 
 import net.panda.core.model.Account;
+import net.panda.core.model.ParameterSummary;
 import net.panda.core.security.SecurityRoles;
+import net.panda.service.security.PipelineFunction;
 import net.panda.test.AbstractIntegrationTest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -15,6 +17,50 @@ import java.util.concurrent.Callable;
 @ContextConfiguration({ "classpath:META-INF/backend-security-aop.xml" })
 public abstract class AbstractSecurityTest extends AbstractIntegrationTest {
 
+    protected static interface ContextCall {
+
+        <T> T call(Callable<T> call) throws Exception;
+    }
+
+    protected static abstract class AbstractContextCall implements ContextCall {
+
+        @Override
+        public <T> T call(Callable<T> call) throws Exception {
+            // Gets the current context
+            SecurityContext oldContext = SecurityContextHolder.getContext();
+            try {
+                // Sets the new context
+                contextSetup();
+                // Call
+                return call.call();
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                // Restores the context
+                SecurityContextHolder.setContext(oldContext);
+            }
+        }
+
+        protected abstract void contextSetup();
+    }
+
+    protected static class UserCall extends AbstractContextCall {
+
+        public UserCall withPipelineGrant(int id, PipelineFunction fn) {
+            // FIXME ACL
+            return this;
+        }
+
+        @Override
+        protected void contextSetup() {
+            SecurityContext context = new SecurityContextImpl();
+            Authentication authentication = new AccountAuthentication(new Account(1, "user", "Normal user",
+                    "user@test.com", SecurityRoles.USER, "builtin", Locale.ENGLISH));
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+        }
+    }
+
 	protected <T> T asAnonymous(Callable<T> call) throws Exception {
 		return withContext(call, new Runnable() {
 			@Override
@@ -24,18 +70,9 @@ public abstract class AbstractSecurityTest extends AbstractIntegrationTest {
 		});
 	}
 
-	protected <T> T asUser(Callable<T> call) throws Exception {
-		return withContext(call, new Runnable() {
-			@Override
-			public void run() {
-				SecurityContext context = new SecurityContextImpl();
-				Authentication authentication = new AccountAuthentication(new Account(1, "user", "Normal user",
-						"user@test.com", SecurityRoles.USER, "builtin", Locale.ENGLISH));
-				context.setAuthentication(authentication);
-				SecurityContextHolder.setContext(context);
-			}
-		});
-	}
+    protected UserCall asUser() {
+        return new UserCall();
+    }
 
 	protected <T> T asAdmin(Callable<T> call) throws Exception {
 		return withContext(call, new Runnable() {
--- panda-backend/src/test/java/net/panda/backend/security/StructureServiceIntegrationSecurityTest.java
+++ panda-backend/src/test/java/net/panda/backend/security/StructureServiceIntegrationSecurityTest.java
@@ -1,8 +1,11 @@
 package net.panda.backend.security;
 
+import net.panda.core.model.ParameterCreationForm;
+import net.panda.core.model.ParameterSummary;
 import net.panda.core.model.PipelineCreationForm;
 import net.panda.core.model.PipelineSummary;
 import net.panda.service.StructureService;
+import net.panda.service.security.PipelineFunction;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
@@ -44,7 +47,7 @@ public class StructureServiceIntegrationSecurityTest extends AbstractSecurityTes
 
 	@Test(expected = AccessDeniedException.class)
 	public void pipeline_create_user_denied() throws Exception {
-		asUser(new Callable<Void>() {
+		asUser().call(new Callable<Void>() {
 			@Override
 			public Void call() throws Exception {
 				structureService.createPipeline(new PipelineCreationForm("pipeline_user", "Cannot create a pipeline"));
@@ -55,14 +58,30 @@ public class StructureServiceIntegrationSecurityTest extends AbstractSecurityTes
 
 	@Test
 	public void pipeline_create_admin_ok() throws Exception {
-		PipelineSummary pipeline = asAdmin(new Callable<PipelineSummary>() {
-			@Override
-			public PipelineSummary call() throws Exception {
-				return structureService.createPipeline(new PipelineCreationForm("pipeline_admin", "OK"));
-			}
-		});
+		PipelineSummary pipeline = createPipeline("pipeline_admin");
 		assertNotNull(pipeline);
 		assertEquals("pipeline_admin", pipeline.getName());
+	}
+
+	@Test
+	public void pipeline_parameter_create_grant_ok() throws Exception {
+		final PipelineSummary pipeline = createPipeline("pipeline_parameter_create");
+		asUser().withPipelineGrant(pipeline.getId(), PipelineFunction.UPDATE).call(new Callable<ParameterSummary>() {
+			@Override
+			public ParameterSummary call() throws Exception {
+				return structureService.createParameter(pipeline.getId(), new ParameterCreationForm("git_remote",
+						"URL for the Git repository", "", false));
+			}
+		});
+	}
+
+	private PipelineSummary createPipeline(final String name) throws Exception {
+		return asAdmin(new Callable<PipelineSummary>() {
+			@Override
+			public PipelineSummary call() throws Exception {
+				return structureService.createPipeline(new PipelineCreationForm(name, "OK"));
+			}
+		});
 	}
 
 }
